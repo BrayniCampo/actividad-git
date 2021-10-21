3aecd27e6306073ef97c16407abb2b1e29076479
Brayni Ocampo <braynivalentina@gmail.com>
2021-10-21 09:07:04 -0500

    commit Se realizó cambios en el archivo ejercitpuul.py

--- ejercitpuul.py
+++ ejercitpuul.py
@@ -1,8 +1,12 @@
-print("Hoy está haciendo un día muy bonit") 
+print("Hoy está haciendo un día muy bonit0") 
 
 def ingresenums():
   lista=[]
-  cuantos(int(input(" ¿Cuántos números desea agregar?" ))
+  cuantos=int(input(" ¿Cuántos números desea agregar?" ))
+  i=0
+  while i<=cuantos:
+    ingrese=int(input("Ingrese un número "))
+    
   lista.append(cuantos)
   print("Los números ingresados son:", lista)
-          
+        
