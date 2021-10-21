ed58392d24749f568af2811f989f744bd44026a7
BrayniCampo <88557215+BrayniCampo@users.noreply.github.com>
2021-10-21 08:55:46 -0500

    Creando un ejercicio para usar el comando git pull

---
+++ ejercitpuul.py
@@ -0,0 +1,8 @@
+print("Hoy está haciendo un día muy bonit") 
+
+def ingresenums():
+  lista=[]
+  cuantos(int(input(" ¿Cuántos números desea agregar?" ))
+  lista.append(cuantos)
+  print("Los números ingresados son:", lista)
+          
